dd74b5dd023c149803242abc252b429e60a496e1
Your Name <you@example.com>
2021-03-17 21:26:46 +0700

    Winform

---
+++ NguyenHoangVuong__5951071124/NguyenHoangVuong__5951071124/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace NguyenHoangVuong__5951071124
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txthosinhvien = new System.Windows.Forms.TextBox();
+            this.txtsbd = new System.Windows.Forms.TextBox();
+            this.txtdiachi = new System.Windows.Forms.TextBox();
+            this.txttensinhvien = new System.Windows.Forms.TextBox();
+            this.txtsdt = new System.Windows.Forms.TextBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnCapnhat = new System.Windows.Forms.Button();
+            this.btnXacLap = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.dgvSinhvien = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSinhvien)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Họ sinh viên:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Số báo danh:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Địa chỉ:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(428, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(135, 25);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Tên sinh viên:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(425, 98);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(132, 25);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Số điện thoại:";
+            // 
+            // txthosinhvien
+            // 
+            this.txthosinhvien.Location = new System.Drawing.Point(168, 38);
+            this.txthosinhvien.Name = "txthosinhvien";
+            this.txthosinhvien.Size = new System.Drawing.Size(242, 30);
+            this.txthosinhvien.TabIndex = 1;
+            // 
+            // txtsbd
+            // 
+            this.txtsbd.Location = new System.Drawing.Point(168, 95);
+            this.txtsbd.Name = "txtsbd";
+            this.txtsbd.Size = new System.Drawing.Size(242, 30);
+            this.txtsbd.TabIndex = 1;
+            // 
+            // txtdiachi
+            // 
+            this.txtdiachi.Location = new System.Drawing.Point(168, 150);
+            this.txtdiachi.Name = "txtdiachi";
+            this.txtdiachi.Size = new System.Drawing.Size(242, 30);
+            this.txtdiachi.TabIndex = 1;
+            // 
+            // txttensinhvien
+            // 
+            this.txttensinhvien.Location = new System.Drawing.Point(569, 40);
+            this.txttensinhvien.Name = "txttensinhvien";
+            this.txttensinhvien.Size = new System.Drawing.Size(242, 30);
+            this.txttensinhvien.TabIndex = 1;
+            // 
+            // txtsdt
+            // 
+            this.txtsdt.Location = new System.Drawing.Point(569, 95);
+            this.txtsdt.Name = "txtsdt";
+            this.txtsdt.Size = new System.Drawing.Size(242, 30);
+            this.txtsdt.TabIndex = 1;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(41, 196);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(133, 58);
+            this.btnThem.TabIndex = 2;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnCapnhat
+            // 
+            this.btnCapnhat.Location = new System.Drawing.Point(180, 196);
+            this.btnCapnhat.Name = "btnCapnhat";
+            this.btnCapnhat.Size = new System.Drawing.Size(133, 58);
+            this.btnCapnhat.TabIndex = 2;
+            this.btnCapnhat.Text = "Cập nhật";
+            this.btnCapnhat.UseVisualStyleBackColor = true;
+            this.btnCapnhat.Click += new System.EventHandler(this.btnCapnhat_Click);
+            // 
+            // btnXacLap
+            // 
+            this.btnXacLap.Location = new System.Drawing.Point(319, 196);
+            this.btnXacLap.Name = "btnXacLap";
+            this.btnXacLap.Size = new System.Drawing.Size(133, 58);
+            this.btnXacLap.TabIndex = 2;
+            this.btnXacLap.Text = "Xác lập";
+            this.btnXacLap.UseVisualStyleBackColor = true;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(458, 196);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(133, 58);
+            this.btnXoa.TabIndex = 2;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(698, 196);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(133, 58);
+            this.btnThoat.TabIndex = 2;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // dgvSinhvien
+            // 
+            this.dgvSinhvien.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvSinhvien.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvSinhvien.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSinhvien.Location = new System.Drawing.Point(12, 272);
+            this.dgvSinhvien.Name = "dgvSinhvien";
+            this.dgvSinhvien.RowHeadersWidth = 51;
+            this.dgvSinhvien.RowTemplate.Height = 24;
+            this.dgvSinhvien.Size = new System.Drawing.Size(819, 241);
+            this.dgvSinhvien.TabIndex = 3;
+            this.dgvSinhvien.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvSinhvien_CellClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(842, 525);
+            this.Controls.Add(this.dgvSinhvien);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnXacLap);
+            this.Controls.Add(this.btnCapnhat);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.txtsdt);
+            this.Controls.Add(this.txttensinhvien);
+            this.Controls.Add(this.txtdiachi);
+            this.Controls.Add(this.txtsbd);
+            this.Controls.Add(this.txthosinhvien);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Student Infomation";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSinhvien)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txthosinhvien;
+        private System.Windows.Forms.TextBox txtsbd;
+        private System.Windows.Forms.TextBox txtdiachi;
+        private System.Windows.Forms.TextBox txttensinhvien;
+        private System.Windows.Forms.TextBox txtsdt;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnCapnhat;
+        private System.Windows.Forms.Button btnXacLap;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.DataGridView dgvSinhvien;
+    }
+}
+
---
+++ NguyenHoangVuong__5951071124/NguyenHoangVuong__5951071124/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NguyenHoangVuong__5951071124
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetStudentsRecord();
+        }
+        SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-DQ9LSA83\SQLEXPRESS;Initial Catalog=DemoCRUD;Integrated Security=True");
+        private void GetStudentsRecord()
+        {
+            SqlCommand cmd = new SqlCommand("select * from StudentsTb",con);
+            DataTable dt = new DataTable();
+            con.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            con.Close();
+            dgvSinhvien.DataSource = dt;
+        }
+        private bool IsValidData()
+        {
+            if(txthosinhvien.Text == string.Empty || txttensinhvien.Text == string.Empty 
+                || txtdiachi.Text == string.Empty || string.IsNullOrEmpty(txtsdt.Text)
+                || string.IsNullOrEmpty(txtsbd.Text))
+            {
+                MessageBox.Show("Vui lòng điền đầy đủ thông tin", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            if (IsValidData())
+            {
+                SqlCommand cmd = new SqlCommand("insert into StudentsTb values (@Name,@FatherName,@RollNumber,@Address,@Mobile)", con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@Name", txthosinhvien.Text);
+                cmd.Parameters.AddWithValue("@FatherName", txttensinhvien.Text);
+                cmd.Parameters.AddWithValue("@RollNumber", txtsbd.Text);
+                cmd.Parameters.AddWithValue("@Address", txtdiachi.Text);
+                cmd.Parameters.AddWithValue("@Mobile", txtsdt.Text);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                GetStudentsRecord();
+            }
+        }
+        public int StudentID;
+        private void dgvSinhvien_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = new DataGridViewRow();
+            row = dgvSinhvien.Rows[e.RowIndex];
+            StudentID = Convert.ToInt32(row.Cells[0].Value);
+            txthosinhvien.Text = row.Cells[1].Value.ToString();
+            txttensinhvien.Text = row.Cells[2].Value.ToString();
+            txtsbd.Text = row.Cells[3].Value.ToString();
+            txtdiachi.Text = row.Cells[4].Value.ToString();
+            txtsdt.Text = row.Cells[5].Value.ToString();
+        }
+
+        private void btnCapnhat_Click(object sender, EventArgs e)
+        {
+            if(StudentID > 0)
+            {
+                SqlCommand cmd = new SqlCommand("update StudentsTb set Name = @Name,FatherName = @FatherName,RollNumber = @RollNumber,Address = @Address,Mobile=@Mobile where StudentID = @ID",con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@Name", txthosinhvien.Text);
+                cmd.Parameters.AddWithValue("@FatherName", txttensinhvien.Text);
+                cmd.Parameters.AddWithValue("@RollNumber", txtsbd.Text);
+                cmd.Parameters.AddWithValue("@Address", txtdiachi.Text);
+                cmd.Parameters.AddWithValue("@Mobile", txtsdt.Text);
+                cmd.Parameters.AddWithValue("@ID", this.StudentID);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                GetStudentsRecord();
+                ResetData();
+            }
+            else
+            {
+                MessageBox.Show("Cập nhật bị lỗi", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ResetData()
+        {
+            txthosinhvien.Text = txttensinhvien.Text = txtsdt.Text = txtsbd.Text = txtdiachi.Text = "";
+
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if(StudentID > 0)
+            {
+                if (MessageBox.Show("Bạn có chắc chắn muốn xóa StudentID " + StudentID + "?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    SqlCommand cmd = new SqlCommand("delete from StudentsTb where StudentID = @ID", con);
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.AddWithValue("@ID", this.StudentID);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    GetStudentsRecord();
+                    ResetData();
+                }
+                else
+                {
+                    MessageBox.Show("Xóa thất bại", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn student cần xóa", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+           
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có chắc chắn muốn thoát ?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                this.Dispose();
+            }
+        }
+    }
+}
